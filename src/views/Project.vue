d36648c7b05bed919c2a7f8ce688188f5bcf789c
Gonçalo Brito <gbritoo.27@hotmail.com>
2019-12-18 23:16:43 +0000

    Add supply video. Minified project 1 and 2 images.

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -7,12 +7,25 @@
       <div v-for="image in project.images" :key="image">
         <img :src="image" alt="" />
       </div>
+      <div v-if="project.id === 6">
+        <!-- Custom video for Supply Project (id = 6) -->
+        <div style="padding:56.25% 0 0 0;position:relative;">
+          <iframe
+            src="https://player.vimeo.com/video/376453725?autoplay=1&loop=1"
+            style="position:absolute;top:0;left:0;width:100%;height:100%;"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import projects from '@/mixins/projects.js'
+import projects from '@/mixins/projects'
+import Vue from 'vue'
 
 export default {
   name: 'project',
@@ -27,6 +40,9 @@ export default {
     project() {
       return this.projects.find(project => project.id === this.id)
     }
+  },
+  created() {
+    Vue.loadScript('https://player.vimeo.com/api/player.js')
   }
 }
 </script>
